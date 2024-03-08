6106bc92243732d1f31dc97c1ce050d2342c43ea
Kothar-2000 <kotharghafarian2000@gmail.com>
2024-03-08 16:39:53 +0330

    Update index.js

--- rating-component/src/index.js
+++ rating-component/src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
+import SubmittedPage from "./SubmittedPage";
+import { RateProvider } from "./context/RateContext";
 
+export default function Index() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="/submit" element={<SubmittedPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RateProvider>
+      <Index />
+    </RateProvider>
   </React.StrictMode>
 );
